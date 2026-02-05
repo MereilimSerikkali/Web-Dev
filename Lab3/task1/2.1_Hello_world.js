bc25917c677e228c6cc5614102f68f75510325d0
MereilimSerikkali <serikkalimereilim@gmail.com>
2026-02-05 16:01:29 +0500

    task1 completed

---
+++ Lab3/task1/2.1_Hello_world.js
@@ -0,0 +1,33 @@
+//task1
+/*
+<!DOCTYPE html>
+<html>
+
+<body>
+
+  <script>
+    alert( "I'm JavaScript!" );
+  </script>
+
+</body>
+
+</html>
+*/
+
+ 
+//task2
+/*
+<!DOCTYPE html>
+<html>
+
+<body>
+
+  <script src="alert.js"></script>
+
+</body>
+
+</html>
+*/
+
+//alert.js
+alert("I'm JavaScript!");
---
+++ Lab3/task1/2.4_variables.js
@@ -0,0 +1,15 @@
+//task1
+let admin, name; 
+name = "John";
+admin = name;
+alert( admin );
+
+//task2
+let ourPlanetName = "Earth";
+let currentUserName = "Merei";
+
+//task3
+// I would change the code like this, so BIRTHDAY is a constant and age could change so it is not in caps
+const BIRTHDAY = '18.04.1982'; 
+
+const age = someCode(BIRTHDAY);
